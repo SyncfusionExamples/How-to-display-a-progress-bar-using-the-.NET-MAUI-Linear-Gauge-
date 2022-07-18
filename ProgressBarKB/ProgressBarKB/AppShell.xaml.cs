5b2df537f37de6a66b972f81ffaf1765b5eba5d4
Shalini-Ashokan <shalini.ashokan@syncfusion.com>
2022-07-18 15:44:18 +0530

    Design a progress bar using linear gauge

---
+++ ProgressBarKB/ProgressBarKB/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgressBarKB;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProgressBarKB/ProgressBarKB/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ProgressBarKB;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
